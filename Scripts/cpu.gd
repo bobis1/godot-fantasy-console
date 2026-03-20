10415dd4ec01cdbd2a5f604e557419a2d71ea5c0
Joseph <josephwaizy@gmail.com>
2026-03-20 07:11:17 -0500

    more progress on cpu

--- Scripts/cpu.gd
+++ Scripts/cpu.gd
@@ -1,5 +1,21 @@
 extends Node
 
+
+var registers = [0, 0, 0, 0, 0, 0, 0, 0]
+var pc = 0
+
+enum {
+	STOP,
+	MOV_R_V,
+	MOV_R_R,
+	WRITE,
+	LOAD,
+	ADD,
+	SUB,
+	JMP,
+	SPR
+}
+
 var AssemblyFile
 var AssemblyFileName: String
 @export var hardware: Script
@@ -21,3 +37,22 @@ func spr(spriteIndex: int, x: int, y: int, w: int = 1, h: int = 1):
 		for x_off in range(w):
 			var current_sprite = spriteIndex + x_off + (y_off * 16)
 			hardware.draw_sprite(current_sprite, x + (x_off * 8), y + (y_off * 8))
+
+func run_cpu():
+	Globals.ram[pc]
+
+
+
+func compile(source_code: String) -> PackedByteArray:
+	var bytecode = PackedByteArray()
+	var lines = source_code.split("\n")
+
+	for line in lines:
+		line = line.strip_edges()
+		if line == "" or line.begins_with(";"): continue # Skip empty lines/comments
+
+		var tokens = line.replace(",", " ").split(" ", false)
+		var command = tokens[0].to_upper()
+
+		
+	return bytecode
